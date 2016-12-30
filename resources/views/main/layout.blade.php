360db2e70d73d80f74258930b368468f5491c470
7timus <7timus@gmail.com>
2016-12-30 09:05:47 +0200

    LATEST SHET

---
+++ resources/views/main/layout.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Skolas Darbs</title>
+    <meta name="description" content="Your Description Here">
+    <meta name="keywords" content="free porn, free money, poker, summer, money, work, cheat">
+    <meta name="author" content="Egons Kalnins">
+    {{-- Stylesheeets--}}
+    {{--===============================================--}}
+    <link rel="stylesheet" href="{{ url('/') }}/css/bootstrap/app.css" >
+    <link rel="stylesheet" href="{{ url('/') }}/css/bootstrap/bootstrap.min.css" >
+    <link rel="stylesheet" href="{{ url('/') }}/css/app.css" >
+    {{-- Skripti--}}
+    {{--===============================================--}}
+    <script src="{{ url('/') }}/js/vendor/jquery.js"></script>
+    <script src="{{ url('/') }}/js/vendor/bootstrap.min.js"></script>
+    <script src="{{ url('/') }}/js/script.js"></script>
+<body>
+{{-- Inversais melnais navigacias bars pielinkots pie augšas--}}
+<nav class="navbar navbar-inverse navbar-fixed-top" id="navigation">
+    <div class="container">
+        <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigacija">
+                <span class="icon-bar"></span> {{-- izveidoju collapse navbaru ar 3 linijām nopointojam bootsrapu uz navigacijas ID--}}
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+            <a href="#" class="navbar-brand"> Websaits</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navigacija">
+            <ul class="nav navbar-nav">
+                <li> <a href="#"> Menu </a></li>
+                <li> <a href="#"> Menu </a></li>
+                <li> <a href="#"> Menu </a></li>
+                <li> <a href="#"> Menu </a></li>
+
+            </ul>
+        </div>
+
+    </div>
+</nav>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1> Mana lapa</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis velit sed lectus vulputate mattis. Sed eu auctor diam. Proin vitae nibh consectetur, blandit sapien sit amet, sagittis nunc. Vivamus cursus cursus magna eget sagittis. Aenean rhoncus nec lacus et facilisis. Pellentesque viverra, dui vel convallis fermentum, magna lectus sagittis libero, non laoreet ipsum dui suscipit risus. Aenean laoreet metus egestas nisl fermentum vestibulum. Nullam rhoncus auctor urna et sodales. Donec pretium mi ligula, faucibus euismod ex consectetur sit amet.
+
+            Vivamus ex enim, tristique sit amet suscipit eget, lacinia eu odio. Vestibulum sed est dui. Donec ornare cursus hendrerit. Integer imperdiet suscipit nulla, nec placerat dolor dictum in. Morbi maximus ipsum aliquam, ullamcorper ex at, malesuada neque. Suspendisse posuere venenatis elementum. Curabitur maximus tristique fringilla. Suspendisse potenti.
+
+            Aliquam sed mauris et nisi dictum ullamcorper ut sit amet orci. Duis et mauris eget metus scelerisque lacinia ac nec sem. Integer lacus lacus, lacinia vitae justo quis, venenatis lacinia nunc. Nullam malesuada, nisl et placerat aliquet, urna turpis vulputate felis, malesuada finibus purus sem ac nisl. Integer ultrices orci nulla, eu egestas nisl rhoncus eget. Morbi sed leo vel orci rhoncus vestibulum fringilla et ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer viverra, nisi ac fermentum mollis, turpis magna varius nisi, interdum elementum augue urna eget massa. Donec dictum ligula eget arcu ornare, viverra sagittis purus venenatis. Maecenas vitae lectus vitae quam malesuada pulvinar vel eu nisl. </p>
+
+        <div class="btn-group">
+            <a href="#" class="btn-lg btn-warning"></a>
+        </div>
+    </div>
+
+</div>
+Teksts
+
+</body>
+</html>
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('main/layout')
 @section('content')
     <h1> This is Main page!</h1>
 @endsection
