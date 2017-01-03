8b6124f763c1778d7525c8da11dd51ab298a011f
7timus <7timus@gmail.com>
2017-01-03 23:57:30 +0200

    TAISAS JAU PA LENAM

---
+++ resources/views/gaujmaliete/main-real.blade.php
@@ -0,0 +1,46 @@
+@extends('gaujmaliete/layout')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="box main-logo">
+            <div class="col-lg-12 text-center">
+                <h2 class="brand-before">
+                    <span class="small">Jāatrod smuks logo attēls</span>
+                </h2>
+                <h1 class="brand-name">Gaujmaliete Mājaslapa</h1>
+                <hr class="tagline-divider">
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                <h2 class="intro-text text-center">
+                    Studenšu Prezidiju konventa koris uzņem jaunas dziedātājas
+                </h2>
+                <hr>
+                <hr class="visible-xs">
+                <p>Aicinām dziedāt gribošās un varošās korporantes pievienoties mūsu radošajam kolektīvam, piesakoties pie kora diriģentes Daces Bites (mob.tel. 22846208).
+                    </br><strong>otrdienās </strong> s!k! Spīdola C!Q! (Dzirnavu iela 34a-5)
+                    </br><strong>ceturtdienās </strong> s!k! Imeria C!Q! (Pulkv.Brieža iela 8-9), pulksten 18:30
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="box">
+            <div class="col-lg-12">
+                <hr>
+                <h2 class="intro-text text-center">Cits foršs raksts
+                </h2>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('gaujmaliete/main');
 });
+Route::get('/test', function () {
+    return view('gaujmaliete/main-real');
+});
 
 
 
